24ce6e02944488fc7ae8f500e174be5c06c0152d
rajrishiav1 <rajrishav12345@gmail.com>
2023-04-19 13:08:24 +0530

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require ("express");
 const app= express();
 const cors= require("cors");
-const PORT =4002;
+const PORT = process.env.PORT || 4002;
 const router = require("./Routes/router")
 require("./db/conn");
 
